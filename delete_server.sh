b948d26d37e29218bb75ad08b9c91da652faadf0
Roman <sunkevu4@gmail.com>
2021-12-09 00:19:51 +0200

    Update delete_server.sh

--- delete_server.sh
+++ delete_server.sh
@@ -4,13 +4,15 @@ if [ $# -ne 1 ]; then echo "Usage: $0 VM_name"; exit 1; fi
 
 VM_NAME=$1
 
-. WRF-UNG.rc # load openstack environment variables
+. /home/roman/WRF-UNG.rc # load openstack environment variables
 
 rm ".ssh/${VM_NAME}.key"
-echo "SSH key for $VM_NAME deleted" >> vm_launching.log
+echo "SSH key for $VM_NAME deleted" >> launching.log
 
 openstack keypair delete ${VM_NAME}
-echo "Openstak keypair for $VM_NAME deleted" >> vm_launching.log
+echo "Openstak keypair for $VM_NAME deleted" >> launching.log
 
 openstack server delete ${VM_NAME}
-echo -e "Openstack server $VM_NAME deleted\n" >> vm_launching.log
+echo -e "Openstack server $VM_NAME deleted\n" >> launching.log
+
+sed -i 's/active/deleted/g' config.json
