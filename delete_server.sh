b8bd9b4b127f2e287caf386e362eb3eb0aca310b
Roman <sunkevu4@gmail.com>
2021-12-11 18:34:37 +0200

    Update delete_server.sh

--- delete_server.sh
+++ delete_server.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then echo "Usage: $0 VM_name"; exit 1; fi
+
+VM_NAME=$1 || VM_NAME=$(cat config.json | grep hostname | cut -d : -f2 | awk -F\" '{print $2}')
+
 set -e # exit on the first error
 
 . /home/roman/WRF-UNG.rc # load openstack environment variables
 TIME=$(date "+%d.%m.%y-%H:%M:%S")
-
-VM_NAME=$(cat config.json | grep hostname | cut -d : -f2 | awk -F\" '{print $2}')
 
 rm ".ssh/${VM_NAME}.key"
 echo "$TIME SSH key for $VM_NAME deleted" >> launching.log
