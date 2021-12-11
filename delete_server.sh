f2900d711cddf98ec4fc9de1e813c49e132f07d6
Synkevych Roman <sunkevu4@gmail.com>
2021-12-12 01:17:25 +0200

    added time var, change delete logic

--- delete_server.sh
+++ delete_server.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then echo "Usage: $0 VM_name"; exit 1; fi
+if [ $# -ne 1 ]; then echo "Usage: $0 hostname or VM name"; exit 1; fi
 
-VM_NAME=$1 || VM_NAME=$(cat config.json | grep hostname | cut -d : -f2 | awk -F\" '{print $2}')
+VM_NAME=$1 # or VM_NAME=$(cat config.json | grep $VM_NAME | cut -d : -f2 | awk -F\" '{print $2}')
+STATUS=`openstack server list | grep $VM_NAME | awk '{ print $6 }'`
 
 set -e # exit on the first error
 
-. /home/roman/WRF-UNG.rc # load openstack environment variables
+. ENV # load openstack environment variables
 TIME=$(date "+%d.%m.%y-%H:%M:%S")
+
+if test -z "$STATUS"; then
+        echo "$VM_NAME VM not found, canceling\n"
+        echo -e "$TIME $VM_NAME VM not found, canceling\n" >> launching.log
+        exit
+fi
+
+echo "VM $VM_NAME found, starting removing"
 
 rm ".ssh/${VM_NAME}.key"
 echo "$TIME SSH key for $VM_NAME deleted" >> launching.log
--- launch_calpuff.sh
+++ launch_calpuff.sh
@@ -2,24 +2,27 @@
 
 TIME=$(date "+%d.%m.%Y-%H:%M:%S")
 
-echo "$TIME starting creating VM" >> launching.log
-
 . Ansible_envs
 mkdir -p .ssh
 
-HASH=`date --utc +%Y%m%d%H%M`; FLAVOR="m1.large"; TIMER=10
+HASH=`date --utc +%Y%m%d%H%M`;
+FLAVOR="m1.large";
+IMAGE="3bfdbb3f-e4e8-428c-b758-75a1dd366f0d";
+TIMER=10;
 
-VM_NAME="calpuff_${FLAVOR/./_}_${HASH}";
-KEY_PATH=.ssh/"${VM_NAME}.key";
-openstack keypair create $VM_NAME >> $KEY_PATH
-chmod 600 .ssh/"${VM_NAME}.key"
+HOSTNAME="calpuff_${FLAVOR/./_}_${HASH}";
+KEY_PATH=.ssh/"${HOSTNAME}.key";
+openstack keypair create $HOSTNAME >> $KEY_PATH
+chmod 600 .ssh/"${HOSTNAME}.key"
+
+echo "$TIME The beginning of the creation of VM $HOSTNAME" >> launching.log
 
 while true; do
    nova boot --flavor $FLAVOR\
-	   --image 3bfdbb3f-e4e8-428c-b758-75a1dd366f0d\
-	   --key-name $VM_NAME\
+	   --image $IMAGE\
+	   --key-name $HOSTNAME\
 	   --security-groups d134acb2-e6bc-4c82-a294-9617fdf7bf07\
-	   $VM_NAME 2>/dev/null
+	   $HOSTNAME 2>/dev/null
    for i in `seq 1 3`; do
       sec=$TIMER
       while [ $sec -ge 0 ]; do
@@ -28,19 +31,19 @@ while true; do
               sleep 1
       done
 
-     STATUS=`openstack server list | grep $VM_NAME | awk '{ print $6 }'`
-     IP=`openstack server list | grep $VM_NAME | awk '{ split($8, v, "="); print v[2]}'`
-     SYSTEM=`openstack server list | grep $VM_NAME | awk '{ print $10 }'`
-    
+     STATUS=`openstack server list | grep $HOSTNAME | awk '{ print $6 }'`
+     IP=`openstack server list | grep $HOSTNAME | awk '{ split($8, v, "="); print v[2]}'`
+     SYSTEM=`openstack server list | grep $HOSTNAME | awk '{ print $10 }'`
+
      if [ "x$STATUS" = "xACTIVE" ]; then
-	     printf "VM $VM_NAME is $STATUS, IP address $IP, system $SYSTEM\n"
+	     printf "VM $HOSTNAME is $STATUS, IP address $IP, image $SYSTEM\n"
       	     printf "To connect use: ssh -i $KEY_PATH ubuntu@$IP\n"
-      	     echo "$TIME VM $VM_NAME is $STATUS, IP address $IP, system $SYSTEM" >> launching.log
+      	     echo "$TIME VM $HOSTNAME is $STATUS, IP address $IP, image $SYSTEM" >> launching.log
        	     echo -e "$TIME To connect use: ssh -i $KEY_PATH ubuntu@$IP\n" >> launching.log
-	           echo -e "{\n   \"hostname\":\"$VM_NAME\",\n   \"ip\":\"$IP\",\n   \"status\":\"active\"\n}" > config.json
+	           echo -e "{\n   \"hostname\":\"$HOSTNAME\",\n   \"ip\":\"$IP\",\n   \"status\":\"active\"\n}" > config.json
      	     exit
      fi
    done
-   printf "Trying to delete VM $VM_NAME with $STATUS status, IP address $IP, system $SYSTEM\n"
-   openstack server delete `openstack server list | grep $VM_NAME | awk '{ print $2 }'`
+   printf "Launching $HOSTNAME failed with status: $STATUS"
+   echo -e "$TIME Launching $HOSTNAME failed with status: $STATUS\n" >> launching.log
 done
